c950a08cfc0647d57ac1f9650476bf5aa42159a2
SiLeader <sileader.dev@gmail.com>
2025-06-12 20:22:16 +0900

    update version

--- backend/docker-build.sh
+++ backend/docker-build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+version=$(cat Cargo.toml | grep version | awk '{print $3}' | tr -d '"')
+
 docker build \
   --push \
   --tag ghcr.io/sileader/diffusion-stash/backend:latest \
-  --tag ghcr.io/sileader/diffusion-stash/backend:v0.1.1 \
+  --tag ghcr.io/sileader/diffusion-stash/backend:v$version \
   .
