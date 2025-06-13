2e93a0d8448e73d5fcd5ccb44fd5daf6326eddb6
SiLeader <sileader.dev@gmail.com>
2025-06-13 12:51:15 +0900

    WIP infinity scroll

--- frontend/src/app/ui/pages/image-gallery/image-gallery.ts
+++ frontend/src/app/ui/pages/image-gallery/image-gallery.ts
@@ -1,10 +1,10 @@
-import {Component, effect, Resource} from '@angular/core';
+import {Component, effect, Resource, signal} from '@angular/core';
 import {MatProgressBar} from "@angular/material/progress-bar";
 import {ImageList} from '../../parts/image-list/image-list';
 import {map} from 'rxjs';
-import {ProductRepository} from '../../../apis/repositories/product-repository';
+import {FetchListOptions, ProductRepository} from '../../../apis/repositories/product-repository';
 import {ActivatedRoute} from '@angular/router';
-import {MultipleProducts} from '../../../apis/data/product';
+import {MultipleProducts, Product} from '../../../apis/data/product';
 import {Title} from '@angular/platform-browser';
 import {Model} from '../../../apis/data/model';
 import {ModelRepository} from '../../../apis/repositories/model-repository';
@@ -20,18 +20,52 @@ import {toSignal} from '@angular/core/rxjs-interop';
   styleUrl: './image-gallery.css'
 })
 export class ImageGallery {
+  private static readonly DEFAULT_LIMIT = 20;
+
   constructor(route: ActivatedRoute, productRepository: ProductRepository, modelRepository: ModelRepository, title: Title) {
     title.setTitle('Image Gallery - Diffusion Stash');
     const id = toSignal(route.params.pipe(map(params => params['id'])));
 
-    this.products = productRepository.fetchListByModelOrWhole(id);
     this.model = modelRepository.fetchByIdOrNull(id);
 
     effect(() => {
-      title.setTitle(`${this.model.value()?.name} gallery - Diffusion Stash`);
+      const offset = this.items().length;
+      const total = this.totalProducts();
+      if (offset + ImageGallery.DEFAULT_LIMIT >= total) {
+        return;
+      }
+
+      const model = this.model.value();
+      const options: FetchListOptions = {
+        offset: offset,
+        limit: ImageGallery.DEFAULT_LIMIT,
+      };
+      this.assignMore(
+        model
+          ? productRepository.fetchListByModelAsync(model.id, options)
+          : productRepository.fetchListAsync(options)
+      ).then();
+    });
+
+    effect(() => {
+      const model = this.model.value();
+      const name = model?.name ?? 'Image';
+      title.setTitle(`${name} gallery - Diffusion Stash`);
     });
   }
 
+  private async assignMore(promise: Promise<MultipleProducts | null>) {
+    const loaded = await promise;
+    if (!loaded) {
+      return;
+    }
+
+    this.items.update(items => [...items, ...loaded.products]);
+  }
+
   model: Resource<Model | null>;
-  products: Resource<MultipleProducts | null>;
+
+  readonly totalProducts = signal(0);
+
+  readonly items = signal<Product[]>([]);
 }
